569e4440d5f23e0d1bbaf1c6d3f11d198620749f
Dayananda Ksh <dayanandaksh39@gmail.com>
2023-12-16 21:25:51 +0530

    created .py file
    added some details for the program

---
+++ regular_season.py
@@ -0,0 +1,5 @@
+# program to calculate the total number of games play in nba regular season
+# each team plays a total of 82 games
+# each team plays 4 games each againts the other 4 theams in their division (So, a total of 16 games)
+# each team plays 3 or 4 games each againts the other 10 teams in their conference for a total of 36 games
+# each team plays 2 games each each againts the other 15 teams of the non-conference for a total of 30 games
